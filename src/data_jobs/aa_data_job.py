07e412d75266b15d299b6c015d6c057fdb4692df
SanjeetShukla01 <sanjeet.shukla089@gmail.com>
2023-01-14 20:28:46 +0530

    adding config files. logging.conf is not used and will be removed

--- src/data_jobs/aa_data_job.py
+++ src/data_jobs/aa_data_job.py
@@ -5,12 +5,20 @@
 # File:             aa_data_job.py
 # -----------------------------------------------------------------------
 from src.app.job import Job
+from src.data_jobs.aa_helper import AirAHelper
 from src.utils import spark_utils
+from src.utils.logging_utils import Logger
 
 
 class AirADataJob(Job):
     def __int__(self, job_name):
         self.job_name = job_name
         self.spark = spark_utils.SparkUtils().get_spark_session("aa_data_job")
+        self.aa_helper = AirAHelper(self.spark)
+
+    logger = Logger(__name__).get_logger()
+    url = "https://randomuser.me/api/0.8/?results=100"
+    json_url = "https://gitlab.com/im-batman/simple-data-assestment/-/raw/main/superman.json"
+
 
 
